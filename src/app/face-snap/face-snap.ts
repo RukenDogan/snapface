d9bf781b87e1f12083e52bbee15fe5c31944bdbc
rukenDogan <ruken.dogan.rkn@gmail.com>
2026-01-05 16:01:54 +0100

    dynamic event binding

--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -12,16 +12,36 @@ export class FaceSnap implements OnInit {
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
 
   ngOnInit(): void {
-      this.title = 'Snapface';
-      this.description = 'A simple Angular app to snap your face';
-      this.createdAt = new Date();
-      this.snaps = 0;
-      this.imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+    this.title = 'Snapface';
+    this.description = 'A simple Angular app to snap your face';
+    this.createdAt = new Date();
+    this.snaps = 0;
+    this.imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+    this.snapButtonText = 'Oh snap!';
+    this.userHasSnapped = false;
   }
 
-  onAddSnap(): void {
+  onSnap(): void {
+    if (this.userHasSnapped) {
+      this.unSnap();
+    } else {
+      this.snap();
+    }
+  }
+
+  unSnap() {
+    this.snaps--;
+    this.snapButtonText = 'Oh snap!';
+    this.userHasSnapped = false;
+  }
+
+  snap() {
     this.snaps++;
+    this.snapButtonText = 'Oops unSnap!';
+    this.userHasSnapped = true;
   }
 }
