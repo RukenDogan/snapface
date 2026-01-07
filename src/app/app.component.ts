7cb9486da48083aa272acdf34cab5d60f8151d3f
rukenDogan <ruken.dogan.rkn@gmail.com>
2026-01-07 14:48:03 +0100

    array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,33 +11,42 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+
+  faceSnaps!: FaceSnap[];
+
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
   
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
+    this.faceSnaps = [
+      new FaceSnap(
       'My first snap',
       'This is my first snap description',
       'https://tse4.mm.bing.net/th/id/OIP.kCydCYlVcKV4uJQTARLJcAHaHT?pid=Api&P=0&w=300&h=300',
       new Date(),
       0
-    );
-    this.myOtherSnap = new FaceSnap(
+      ),
+
+      new FaceSnap(
       'My second snap',
       'This is my second snap description',
       'https://imgix.ranker.com/user_node_img/50147/1002928852/original/1002928852-photo-u-27910876?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=200',
       new Date(),
       78
-    );
-    this.myLastSnap = new FaceSnap(
+    ),
+    new FaceSnap(
       'My last snap',
       'This is my last snap description',
       'https://i.pinimg.com/originals/af/e2/85/afe28514458b03c68eea289007a326e6.jpg',
       new Date(),
       26
-    );
+    ),
 
-    this.myOtherSnap.setLocation('à la montagne');
+    ];
+
+
+
+    this.faceSnaps[1].setLocation('à la montagne');
   }
 }
