2225176a4aa7b37857ad6e76d535a2e21c4e4451
rukenDogan <ruken.dogan.rkn@gmail.com>
2026-01-12 14:50:25 +0100

    new component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { FaceSnapList } from './face-snap-list/face-snap-list';
 import { LandingPage } from './landing-page/landing-page';
+import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
 
 export const routes: Routes = [
+    { path: 'facesnaps/:id', component: SingleFaceSnapComponent },
     { path: 'facesnaps', component: FaceSnapList },
     { path: '', component: LandingPage}
 ];
---
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
+import { NgClass, NgStyle, UpperCasePipe, DatePipe } from '@angular/common';
+import { FaceSnapsService } from '../services/face-snaps.service';
+
+
+@Component({
+  selector: 'app-single-face-snap',
+  imports: [
+    NgStyle,
+    NgClass,
+    UpperCasePipe,
+    DatePipe
+    ],
+  templateUrl: './single-face-snap.component.html',
+  styleUrl: './single-face-snap.component.scss',
+})
+export class SingleFaceSnapComponent {
+  @Input() faceSnap!: FaceSnap;
+
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
+
+  constructor(private faceSnapsService: FaceSnapsService) {}
+
+
+
+ngOnInit(): void {
+  this.snapButtonText = 'Oh snap!';
+  this.userHasSnapped = false;
+}
+
+
+  onSnap(): void {
+    if (this.userHasSnapped) {
+      this.unSnap();
+    } else {
+      this.snap();
+    }
+  }
+
+  unSnap() {
+    this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
+    this.snapButtonText = 'Oh snap!';
+    this.userHasSnapped = false;
+  }
+
+  snap() {
+    this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
+    this.snapButtonText = 'Oops unSnap!';
+    this.userHasSnapped = true;
+  }
+}
